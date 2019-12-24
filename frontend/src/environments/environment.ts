731e2589af7f2a983a0d036abeb223bb589153bf
odunayo <okebunmiodunayo@gmail.com>
2019-12-24 14:17:42 +0100

    update token and frontend folder

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'dev-pai0lgcg', // the auth0 domain prefix
+    audience: 'dev', // the audience set for the auth0 app
+    clientId: 'Vl0lIqq5S3cS22S5ZWjFPt1gc87jpCGe', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
